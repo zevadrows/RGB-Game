4745187bab2709a7bb871c36e4c7349530ef7ba2
Zeva PC <zevadrows@gmail.com>
2019-04-15 22:24:11 -0300

    trying to make every square turn into the right color when rightSquare is clicked

--- app.js
+++ app.js
@@ -7,47 +7,51 @@ var squares = document.querySelectorAll(".square");
 
 
 function rgbGenerator() {
-    var min=0; 
-    var max=255;  
-    var random =Math.floor(Math.random() * (+max - +min)) + +min;
+    var min = 0;
+    var max = 255;
+    var random = Math.floor(Math.random() * (+max - +min)) + +min;
     return random;
 }
 
-function randomNumber6(){
-    var min=0; 
-    var max=5;  
-    var random =Math.floor(Math.random() * (+max - +min)) + +min;
+function randomNumber6() {
+    var min = 0;
+    var max = 5;
+    var random = Math.floor(Math.random() * (+max - +min)) + +min;
     return random;
 };
 
-function colorGenerate(){
-    return "("+rgbGenerator()+", "+rgbGenerator()+", "+rgbGenerator()+")"
+function colorGenerate() {
+    return "(" + rgbGenerator() + ", " + rgbGenerator() + ", " + rgbGenerator() + ")"
 };
 
-function randomSquares(){
-    for (var i = 0; i<squares.length; i++){
-        squares[i].style.backgroundColor = "rgb"+colorGenerate();
+function randomSquares() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = "rgb" + colorGenerate();
     }
 };
 
-function newColor(){
-    var color = colorGenerate();  
+function newColor() {
+    var color = colorGenerate();
     rgbDisplay.textContent = color;
-    //take out this code later
-    //activeDiff.style.backgroundColor = "rgb"+color;
     randomSquares();
     var randNum = randomNumber6();
     var rightSquare = squares[randNum];
-    rightSquare.style.backgroundColor = "rgb"+color;
-    for (var i = 0; i<squares.length; i++){
-        if (squares[i] == rightSquare){
-            squares[i].addEventListener("click", function(){
+    rightSquare.style.backgroundColor = "rgb" + color;
+    for (var i = 0; i < squares.length; i++) {
+        if (squares[i] == rightSquare) {
+            squares[i].addEventListener("click", function () {
+                /* Trying to make every square turn into rightSquare color
+                for (var i = 0; i < squares.length; i++) {
+                    squares[i].style.backgroundColor = rightSquare.style.backgroundColor;
+                }
+                */
                 rgbDisplay.textContent = "SUCCESS";
             });
         }
         else {
-            squares[i].addEventListener("click", function(){
-                rgbDisplay.textContent = "NOT!!!";
+            squares[i].addEventListener("click", function () {
+                
+                rgbDisplay.textContent = "WRONG!!!";
             });
         }
     }
@@ -55,7 +59,7 @@ function newColor(){
 
 newColor();
 
-newColorBtn.addEventListener("click", function(){
+newColorBtn.addEventListener("click", function () {
     newColor();
 });
 
